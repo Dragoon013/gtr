fb557501d9f92e3daf2435ce563644fd649b9ca6
jteal <jacob.a.teal@gmail.com>
2014-12-05 12:36:07 -0300

    decided to obscure all for now, basic functionality is there though. may update drawing later

--- game.js
+++ game.js
@@ -8,10 +8,11 @@ var BOARD_W;
 var BOARD_H;
 
 var tiles = [];
+var pix = [];
 
 var BOARD = [[1,2,3],
-	         [4,5,6],
-	         [7,8,9]];
+	     [4,5,6],
+	     [7,8,9]];
 
 var game = (function(){
     var tiles = [];
@@ -31,6 +32,19 @@ var game = (function(){
 
     return{
 	    init: function(){
+
+		rm.init();
+		
+		rm.addResource("yel", "/images/Photos/Yel.jpg", "jpg", rm.ResourceType.IMAGE);
+		rm.addResource("vaultboy", "/images/vaultboy.png", "png", rm.ResourceType.IMAGE);
+		rm.addResource("joaquin", "/images/Photos/Joaquin.jpg", "jpg", rm.ResourceType.IMAGE);
+		rm.addResource("peter", "/images/Photos/PeterKo.jpg", "jpg", rm.ResourceType.IMAGE);
+		rm.addResource("kendall", "/images/Photos/Kendall.jpg", "jpg", rm.ResourceType.IMAGE);
+		rm.addResource("jesse", "/images/Photos/Jesse.jpg", "jpg", rm.ResourceType.IMAGE);
+		rm.addResource("rob", "/images/Photos/Rob.jpg", "jpg", rm.ResourceType.IMAGE);
+		rm.addResource("tato", "/images/Photos/Tato.jpg", "jpg", rm.ResourceType.IMAGE);
+		
+		rm.startPreloading();
 	        canvas.addEventListener('click',function(e){
 		        game.click(e);
 	        });
@@ -40,52 +54,57 @@ var game = (function(){
 	    },
 
 	    click: function(e){
-            var x = e.x;
-            var y = e.y;
-
-            // Firefox (im not sure this works)
+		var x = e.x;
+		var y = e.y;
+		
+            // Firefox
             if (!x || !y) {
                 var x = e.clientX + document.body.scrollLeft +
                     document.documentElement.scrollLeft;
                 var y = e.clientY + document.body.scrollTop +
                     document.documentElement.scrollTop;
             }
-
-            // Align x,y with the canvas
+		// Align x,y with the canvas
 	        x -= canvas.offsetLeft;
-            y -= canvas.offsetTop;
+		y -= canvas.offsetTop;
 
             // Snap x,y to grid corners where a tile's x,y will be
 	        var rx = Math.floor(x / SIZE) * SIZE;
 	        var ry = Math.floor(y / SIZE) * SIZE;
 
+		console.log(rx, ry);
             // Find the tile we just clicked and flip it
+            // this here is pretty
             tiles.map(function(t) {
                 if (t.x === rx && t.y === ry)
                     t.flipped = true;
-                else
-                    t.flipped = false;
+                //else
+                //    t.flipped = false;
             });
-
 
             // Redraw game board
             game.draw();
 	    },
 
 	    template_mapper: function(template){
+
 	        for (var i = 0; i < template.length; i++){
 		        for (var j = 0; j < template[i].length; j++){
 		            tiles.push(new Tile(i * SIZE, j * SIZE));
-		        }
+		            pix.push(new Sprite('picture', j*SIZE, i*SIZE, new Pix(rm.images["vaultboy"],SIZE,SIZE)));
+			}
+
 	        }
 	    },
 
         // Draw all tiles
 	    draw: function(){
-            context.clearRect(0, 0, BOARD_W, BOARD_H);
+	    context.clearRect(0, 0, BOARD_W, BOARD_H);
+		pix.map(function(p){p.artist.draw(p,context)});
 	        for (var i = 0; i < tiles.length; i++){
 		        tiles[i].draw(context);
 	        }
+
 	    }
     }
 })();
--- tiles.js
+++ tiles.js
@@ -4,19 +4,49 @@ var Tile = function (x, y){
     this.y = y;
     this.width = SIZE;
     this.flipped = false;
-    this.padding = 2;
+    this.padding = 1;
 };
 
 Tile.prototype = {
     draw: function (context) {
         // Setup
         context.save();
-        context.globalAlpha = .6;
+        context.globalAlpha = 1;
         context.fillStyle = 'black';
-        if (this.flipped) context.fillStyle = 'blue';
-
+        if (this.flipped) context.globalAlpha = .0;
         // Drawing
-	    context.fillRect(this.x+this.padding, this.y+this.padding, this.width-2*this.padding, this.width-2*this.padding);
+//	context.fillRect(this.x+this.padding, this.y+this.padding, this.width-1*this.padding, this.width-1*this.padding);
+	context.fillRect(this.x, this.y,this.width, this.width);
         context.restore();
     }
 };
+
+var Pix = function(img, w, h){
+    this.img = img;
+    this.w = w;
+    this.h = h;
+    this.show = false;
+};
+
+Pix.prototype = {
+    draw: function (sprite,context){
+
+	context.drawImage(this.img,
+			  sprite.x,sprite.y, 
+			  this.w, this.h, 
+			  sprite.x, sprite.y, 
+			  this.w, this.h);
+	
+
+    }
+};
+
+var Sprite = function(type, x, y, artist){
+    this.artist = artist;
+    this.type = type;
+    this.x = x;
+    this.y = y;
+ 
+    
+};
+    
